7256a6d3bad90ba5b471f4dd8d917bb00bc793fb
Lucas Napoli <lucas.napoli@gmail.com>
2019-07-31 16:53:21 -0400

    Credentials and auth set

--- src/main/java/com/library/search/book/SearchService.java
+++ src/main/java/com/library/search/book/SearchService.java
@@ -1,9 +1,12 @@
 package com.library.search.book;
 
+import sun.misc.BASE64Decoder;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -20,7 +23,37 @@ public class SearchService {
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
     @Path( "/list/" )
-    public ArrayList<Book> bookSearch (Book book) throws Exception {
-        return BookRepository.getInstance ( context ).searchBook (book);
+    public ArrayList<Book> bookSearch (@HeaderParam("authorization")String auth, Book book)  {
+        if (credentialValidation(auth)) {
+            try {
+                return BookRepository.getInstance(context).searchBook(book);
+            } catch (Exception ex) {
+                System.out.println("Error: " + ex);
+            }
+        }
+        return null;
     }
+
+    public Boolean credentialValidation (String auth) {
+        String decodeAuth = "";
+        String[] authParts = auth.split("\\s+");
+        String authInfo = authParts[1];
+        byte[] bytes = null;
+        try {
+            bytes = new BASE64Decoder().decodeBuffer(authInfo);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Error: " + e);
+        }
+        decodeAuth = new String(bytes);
+
+        String [] userCredentials = decodeAuth.split(":");
+            if (userCredentials[0].equals("book-service") && userCredentials.equals("book-service-secure-password- 1234")){
+                return true;
+            } else {
+                return false;
+            }
+    }
+
+
 }
